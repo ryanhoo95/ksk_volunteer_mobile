0a2efbda6c18422124fedfde769254e03fc50b4d
ryanhoo <hbkeong95@gmail.com>
2018-03-17 16:06:35 +0800

    join and withdraw activity

--- src/pages/activity-details/activity-details.ts
+++ src/pages/activity-details/activity-details.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, App } from 'ionic-angular';
+import { KskProvider } from '../../providers/ksk/ksk';
 
 /**
  * Generated class for the ActivityDetailsPage page.
@@ -14,11 +15,16 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'activity-details.html',
 })
 export class ActivityDetailsPage {
+  token: any;
   activity: any;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private kskProvider: KskProvider, private app: App) {
+    this.kskProvider.getSessionData("token").then((val) => {
+      this.token = val;
+    });
+
     this.activity = navParams.get('activity');
-    console.log(this.activity);
+    // console.log(this.activity);
   }
 
   ionViewDidLoad() {
@@ -35,11 +41,73 @@ export class ActivityDetailsPage {
   }
 
   joinActivity() {
-    console.log("join");
+    // console.log("join");
+
+    this.kskProvider.showProgress();
+
+    let params = {
+      "api_token" : this.token,
+      "activity_id" : this.activity.activity_id,
+      "participation_id" : this.activity.participation_id
+    };
+
+    this.kskProvider.postData(params, "joinActivity").then((result) => {
+      let response: any = result;
+      console.log(response);
+
+      this.kskProvider.dismissProgress();
+
+      if(response.status == "success") {
+        this.kskProvider.showAlertDialog("Success", response.message);
+        this.navCtrl.pop();
+      }
+      else if(response.status == "invalid") {
+        this.kskProvider.showAlertDialog("Fail", response.message);
+        this.kskProvider.clearSessionData();
+        this.app.getRootNav().setRoot('LoginPage');
+      }
+      else {
+        this.kskProvider.showAlertDialog("Fail", response.message);
+      }
+
+    }, (err) => {
+      this.kskProvider.dismissProgress();
+      this.kskProvider.showServerErrorDialog();
+    });
   }
 
   withdrawActivity() {
-    console.log("withdraw");
+    // console.log("withdraw");
+    this.kskProvider.showProgress();
+
+    let params = {
+      "api_token" : this.token,
+      "participation_id" : this.activity.participation_id
+    };
+
+    this.kskProvider.postData(params, "withdrawActivity").then((result) => {
+      let response: any = result;
+      console.log(response);
+
+      this.kskProvider.dismissProgress();
+
+      if(response.status == "success") {
+        this.kskProvider.showAlertDialog("Success", response.message);
+        this.navCtrl.pop();
+      }
+      else if(response.status == "invalid") {
+        this.kskProvider.showAlertDialog("Fail", response.message);
+        this.kskProvider.clearSessionData();
+        this.app.getRootNav().setRoot('LoginPage');
+      }
+      else {
+        this.kskProvider.showAlertDialog("Fail", response.message);
+      }
+
+    }, (err) => {
+      this.kskProvider.dismissProgress();
+      this.kskProvider.showServerErrorDialog();
+    });
   }
 
 }
