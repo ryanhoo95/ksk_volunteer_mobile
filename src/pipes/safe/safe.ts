c95cc1e0907afad84ab496d340166f5e5b77add9
ryanhoo <hbkeong95@gmail.com>
2018-03-30 22:33:27 +0800

    add innerhtml

---
+++ src/pipes/safe/safe.ts
@@ -0,0 +1,18 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+
+/**
+ * Generated class for the SafePipe pipe.
+ *
+ * See https://angular.io/api/core/Pipe for more info on Angular Pipes.
+ */
+@Pipe({
+  name: 'safe',
+})
+export class SafePipe implements PipeTransform {
+  constructor(protected sanitizer: DomSanitizer) {}
+
+  transform(htmlString: string): any {
+    return this.sanitizer.bypassSecurityTrustHtml(htmlString);
+  }
+}
