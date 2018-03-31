261855aba4af3d9fdc63dfab8560d2408e87d2f8
ryanhoo <hbkeong95@gmail.com>
2018-03-31 23:14:49 +0800

    add pipe to vip remark

--- src/pages/participation-details/participation-details.module.ts
+++ src/pages/participation-details/participation-details.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { ParticipationDetailsPage } from './participation-details';
+import { PipesModule } from '../../pipes/pipes.module';
 
 @NgModule({
   declarations: [
@@ -8,6 +9,7 @@ import { ParticipationDetailsPage } from './participation-details';
   ],
   imports: [
     IonicPageModule.forChild(ParticipationDetailsPage),
+    PipesModule
   ],
   exports: [
     ParticipationDetailsPage
