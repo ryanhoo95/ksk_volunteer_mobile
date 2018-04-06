38bbaf32fe912fa77e962321706eab0fbc972117
ryanhoo <hbkeong95@gmail.com>
2018-04-06 14:53:38 +0800

    fix register bug

--- src/pages/register/register.ts
+++ src/pages/register/register.ts
@@ -215,6 +215,7 @@ export class RegisterPage {
 
       if(response.status == "success") {
         this.kskProvider.setSessionData('token', response.data.api_token);
+        this.kskProvider.setSessionData('usertype', response.data.usertype);
         // this.navCtrl.push('TabsPage');
         this.navCtrl.setRoot('TabsPage');
       }
