7b2457b45bcdf8d9a726cae210dc30b03a7c0f29
ryanhoo <hbkeong95@gmail.com>
2018-04-06 11:26:24 +0800

    add backend function to contact page

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { CallNumber } from '@ionic-native/call-number';
+import { LaunchNavigator, LaunchNavigatorOptions } from '@ionic-native/launch-navigator';
+import { KskProvider } from '../../providers/ksk/ksk';
 
 /**
  * Generated class for the ContactPage page.
@@ -15,11 +18,27 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class ContactPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private callNumber: CallNumber, private launchNavigator: 
+  LaunchNavigator, private kskProvider: KskProvider) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ContactPage');
+  }
+
+  navigate() {
+    let options: LaunchNavigatorOptions = {
+      destinationName: "Kechara Soup Kitchen"
+    };
+
+    this.launchNavigator.navigate([3.1451213, 101.7131014,], options)
+      .catch(() => this.kskProvider.showAlertDialog("Fail", "Error while selecting navigator."));
+  }
+
+  callUs() {
+    this.callNumber.callNumber("03-2141 6046", false)
+      .then(() => console.log("dialing"))
+      .catch(() => this.kskProvider.showAlertDialog("Fail", "Your device does not support calling or SIM card is not present in your device."));
   }
 
 }
